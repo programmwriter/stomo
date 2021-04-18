70f70c2221ad9aea359c975cf46f6aca2732bb74
unknown <shportaklev@mail.ru>
2021-04-18 11:35:47 +0500

    add config

---
+++ config.js
@@ -0,0 +1,74 @@
+// "puppeteer": "^1.20.0",
+
+let conf = {
+  url1: {
+    number: 1,
+    url:
+      "https://rmis.registratura96.ru/#!/group/department_34860/service/3711482/resource/36031636/planning/2021/05/!/",
+    name: "Обвинцева",
+    client: "Кудинов Алексей",
+    polis: "6650320846001107",
+    day: "03",
+    month: "09",
+    year: "1976",
+    phone: "9320147585",
+  },
+  url2: {
+    number: 2,
+    url:
+      "https://rmis.registratura96.ru/#!/group/department_34860/service/3711482/resource/48048316/planning/2021/05/!/",
+    name: "Сержанина",
+    client: "Кудинов Алексей",
+    polis: "6650320846001107",
+    day: "03",
+    month: "09",
+    year: "1976",
+    phone: "9320147585",
+  },
+  url3: {
+    number: 3,
+    url:
+      "https://rmis.registratura96.ru/#!/group/department_34860/service/3712098/resource/26627602/planning/2020/09/!/",
+    name: "Решетникова",
+    client: "Широкова Ульяна",
+    polis: "6689599791000868",
+    day: "08",
+    month: "10",
+    year: "2004",
+    phone: "+79089178522",
+  },
+  url4: {
+    url:
+      "https://rmis.registratura96.ru/#!/group/department_34860/service/3712098/resource/26624621/planning/2020/01/!/",
+    name: "Королькова",
+    client: "Плотниковы",
+    polis: "6691789727000134",
+    day: "22",
+    month: "08",
+    year: "2012",
+    phone: "+79043828393",
+  },
+  url5: {
+    url:
+      "https://rmis.registratura96.ru/#!/group/department_34860/service/3712096/resource/150764766/planning/2019/11/!/",
+    name: "Чалышева",
+    client: "Угрюмова Ольга Ивановна",
+    polis: "6651140883001136",
+    day: "16",
+    month: "08",
+    year: "1958",
+    phone: "+79220313559",
+  },
+  url6: {
+    url:
+      "https://rmis.registratura96.ru/#!/group/department_34860/service/3712097/resource/26588806/planning/2020/01/!/",
+    name: "Игумнова",
+    client: "Плотников Павел Сергеевич",
+    polis: "6691789727000134",
+    day: "22",
+    month: "08",
+    year: "2012",
+    phone: "+79043828393",
+  },
+};
+module.exports = conf;
